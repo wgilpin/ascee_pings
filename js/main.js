52986b6e2807665fa9f159102764ae1a2dd6653f
wgilpin <wgilpin@gmail.com>
2018-10-02 12:11:32 +0100

    copy to clipboard

--- js/main.js
+++ js/main.js
@@ -1,37 +1,36 @@
 $(function() {
   // Handler for .ready() called.
-  $("#sectionOut").hide();
-  function optText (label, id){
+  $('#sectionOut').hide();
+  function optText(label, id) {
     var text = $(id).val();
-    if (text){
-      return label + ': ' + text + '\n'
+    if (text) {
+      return label + ': ' + text + '\n';
     } else {
       return '';
     }
   }
 
-  function copyPing() {
-    /* Get the text field */
-    var $copyText =$("#outText");
-  
-    /* Select the text field */
-    $copyText.select();
-  
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-  
-  }
+  $('#btnCopy').on('click', function(){
+    $('#outText').select();
+    document.execCommand('copy');
+  });
 
-  $("#btnGenerate").on('click', function(){
-    var ping = "@reqbcast :siren: "+
-      $("#inHurf").val()+'\n\n'+
-      optText('FC Name', '#inFC')+
-      optText('Fleet Name', '#inFleetName')+
-      optText('Reimbursement', '#inReimb')+
-      optText('Comms', '#inComms')+
+  $('#btnGenerate').on('click', function() {
+    var ping =
+      '@reqbcast :siren: ' +
+      $('#inHurf').val() +
+      '\n\n' +
+      optText('FC Name', '#inFC') +
+      optText('Fleet Name', '#inFleetName') +
+      optText('Reimbursement', '#inReimb') +
+      optText('Comms', '#inComms') +
       optText('Doctrine', '#inDoctrine');
-    $("#outText").text(ping);
-    copyPing()
-    $("#sectionOut").show();
-  })
+    $('#outText')
+      .text(ping)
+      .select();
+    document.execCommand('copy');
+    $('#sectionOut').show();
+  $('#sectionOut').show();
+
+  });
 });
